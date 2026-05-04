6454bde392b40861d32e592f16d443a3b69a98d2
sebastiian06 <jsagudeloq@corhuila.edu.co>
2026-05-04 15:43:35 -0500

    fix: HU-DEV-JSA-15 corregir funciones y triggers de inventario

--- 05_rollbacks/01_ddl/06_functions/001_update_stock_functions.rollback.sql
+++ 05_rollbacks/01_ddl/06_functions/001_update_stock_functions.rollback.sql
@@ -1,4 +1,4 @@
 -- Eliminar funciones
-DROP FUNCTION IF EXISTS inventario.f_update_stock_on_insert();
-DROP FUNCTION IF EXISTS inventario.f_update_stock_on_update();
-DROP FUNCTION IF EXISTS inventario.f_revert_stock_on_delete();
+DROP FUNCTION IF EXISTS inventario.f_update_stock_on_insert() CASCADE;
+DROP FUNCTION IF EXISTS inventario.f_update_stock_on_update() CASCADE;
+DROP FUNCTION IF EXISTS inventario.f_revert_stock_on_delete() CASCADE;
